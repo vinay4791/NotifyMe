3fc35c2a257051b393aec494d34227b6f2db8b76
Vinay Mathew John <vinay.john@litmus7.com>
2020-04-23 11:09:19 +0530

    Implemented update and cancel for notifications

--- app/src/main/java/vinay/com/notifyme/MainActivity.java
+++ app/src/main/java/vinay/com/notifyme/MainActivity.java
@@ -7,14 +7,18 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
     private static final String PRIMARY_CHANNEL_ID = "notification_channel";
     private NotificationManager mNotifyManager;
     private static final int NOTIFICATION_ID = 0;
@@ -24,24 +28,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_notify = findViewById(R.id.notify);
-        button_notify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendNotification();
-            }
-        });
+        initialize();
+
         createNotificationChannel();
     }
 
-    //sending notification
+    //function for initializing UI elements
+    private void initialize() {
+        button_notify = findViewById(R.id.notify);
+        button_update = findViewById(R.id.update);
+        button_cancel = findViewById(R.id.cancel);
+        button_notify.setOnClickListener(this);
+        button_update.setOnClickListener(this);
+        button_cancel.setOnClickListener(this);
+
+    }
+
+    //function for sending notification
     private void sendNotification() {
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
         mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
+    //function for updating notification
+    public void updateNotification() {
+
+        //converting drawable into a bitmap
+        Bitmap mascotImage  = BitmapFactory
+                .decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(mascotImage)
+                .setBigContentTitle("Notification Updated!"));
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+
+    }
+
+    //function for cancelling notification
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+    }
+
     //Creating a notification channel
-    private  void createNotificationChannel() {
+    private void createNotificationChannel() {
         mNotifyManager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -57,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //creating a notificationbuider
-    private NotificationCompat.Builder getNotificationBuilder(){
+    private NotificationCompat.Builder getNotificationBuilder() {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
                 NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -72,5 +102,24 @@ public class MainActivity extends AppCompatActivity {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
         return notifyBuilder;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.notify:
+                sendNotification();
+                break;
+
+            case R.id.cancel:
+                cancelNotification();
+                break;
+
+            case R.id.update:
+                updateNotification();
+                break;
+
+        }
+
     }
 }
